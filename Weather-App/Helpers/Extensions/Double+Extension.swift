b7e24f61bfd60183ba0a28dd1e3f6841f0762cf4
utkubalkir <devucb01@gmail.com>
2022-03-11 01:22:14 +0300

    chore: convert celcius extension

---
+++ Weather-App/Helpers/Extensions/Double+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Int+Extension.swift
+//  Weather-App
+//
+//  Created by Utku Can BALKIR on 10.03.2022.
+//
+
+import Foundation
+extension Double {
+    var kelvinToCelcius: Double {
+        return self - 273.15
+    }
+}
