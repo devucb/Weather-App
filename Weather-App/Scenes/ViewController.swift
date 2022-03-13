932f2d8abba8c6f8829d901ea555f1a2a46dd270
utkubalkir <devucb01@gmail.com>
2022-03-13 03:33:29 +0300

    feat: get weather by coordinate

--- Weather-App/Scenes/ViewController.swift
+++ Weather-App/Scenes/ViewController.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, ObservableObject, CLLocationManagerDelegate {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
@@ -16,42 +16,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var minTemperature: UILabel!
     @IBOutlet weak var pressureLabel: UILabel!
     @IBOutlet weak var feelsLike: UILabel!
-    
-    //MARK: - Properties
+    // MARK: - Properties
     private let viewModel = ViewModel()
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        viewModel.fetchWeather { [weak self] in
+        viewModel.bind = { [unowned self] in
             DispatchQueue.main.async {
-                self?.setupUI()
-               
+                self.setupUI()
             }
         }
     }
-    //MARK: - UI Configuration
-    
+    // MARK: - UI Configuration
     private func setupUI() {
        setupHeader()
        setupSubHeader()
-        
     }
     private func setupHeader() {
         temperatureLabel.text = viewModel.temperatureString
         locationLabel.text = viewModel.locationString
     }
-    
-    private func  setupSubHeader() {
+    private func setupSubHeader() {
         feelsLike.text = viewModel.feelsLikeString
         maxTemperature.text = viewModel.tempMaxString
         minTemperature.text = viewModel.tempMinString
         humidityLabel.text = viewModel.humidityString
         pressureLabel.text = viewModel.pressureString
     }
-
-
 }
-
--- Weather-App/Scenes/ViewModel.swift
+++ Weather-App/Scenes/ViewModel.swift
@@ -6,22 +6,34 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewModel {
-    // MARK: - Response
-    
-    var weather: WeatherModel?
-    
-    
+class ViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+   
+    private var location: CLLocation = CLLocation(latitude: 41.015137, longitude: 28.979530) {
+        didSet {
+            fetchWeather()
+        }
+    }
+    var weather: WeatherModel? {
+        didSet {
+            bind?()
+        }
+    }
+    let locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        checkLocationController()
+        
+    }
     // MARK: - Properties
     var temperatureString: String {
         return displayableCelciusTemperature(weather?.main.temp ?? 0)
     }
-    
     var locationString: String {
         return String(weather?.name ?? "New York, USA")
     }
-    
     var feelsLikeString: String {
         return displayableCelciusTemperature(weather?.main.feelsLike ?? 0)
     }
@@ -29,32 +41,37 @@ class ViewModel {
     var tempMaxString: String {
         return displayableCelciusTemperature(weather?.main.tempMax ?? 0)
     }
-    
     var tempMinString: String {
         return displayableCelciusTemperature(weather?.main.tempMin ?? 0)
     }
-    
     var humidityString: String {
         return String(weather?.main.humidity ?? 0)
     }
-    
     var pressureString: String {
         return String(weather?.main.pressure ?? 0)
     }
-    
+
     // MARK: - Helpers
-    
     private func displayableCelciusTemperature(_ temperatureAsKelvin: Double) -> String {
-        String(format:"%.1f", temperatureAsKelvin.kelvinToCelcius) + "° C"
+        String(format: "%.1f", temperatureAsKelvin.kelvinToCelcius) + "° C"
     }
-    
+    var bind: (() -> Void)?
     // MARK: - Requests
-    
-    func fetchWeather(for cityId: Int = 5128581, _ completion: @escaping(() -> Void)) {
-        NetworkController.fetchWeather(for: cityId) { weather in
+    func fetchWeather() {
+        NetworkController.fetchWeather(lat: location.coordinate.latitude, lon: location.coordinate.longitude) { weather in
             self.weather = weather
-            completion()
-            
         }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let latestLocation = locations.first else { return }
+        DispatchQueue.main.async {
+         self.location = latestLocation
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    func checkLocationController() {
+        locationManager.requestLocation()
     }
 }
